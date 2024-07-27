3e3ec21cb90819882b79774b83b5aaef743bc187
Mahad Abdullahi Ahmed <59744373+mahad-a@users.noreply.github.com>
2024-07-27 16:26:03 -0400

    necessary Keil files

---
+++ Objects/main.d
@@ -0,0 +1,12 @@
+.\objects\main.o: main.c
+.\objects\main.o: D:\Arm\Packs\TexasInstruments\MSP432P4xx_DFP\3.2.6\Device\Include\msp.h
+.\objects\main.o: D:\Arm\Packs\TexasInstruments\MSP432P4xx_DFP\3.2.6\Device\Include\msp432p401r.h
+.\objects\main.o: C:\Keil_v5\ARM\ARM5.06u7\Bin\..\include\stdint.h
+.\objects\main.o: D:\Arm\Packs\TexasInstruments\MSP432P4xx_DFP\3.2.6\Device\Include\msp_compatibility.h
+.\objects\main.o: D:\Arm\Packs\TexasInstruments\MSP432P4xx_DFP\3.2.6\Device\Include\msp432p401r_classic.h
+.\objects\main.o: D:\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h
+.\objects\main.o: D:\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: D:\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: D:\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: D:\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main.o: D:\Arm\Packs\TexasInstruments\MSP432P4xx_DFP\3.2.6\Device\Include\system_msp432p401r.h
